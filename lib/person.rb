27da3475d01f981af5e01aa77478ee15cc9bbdbf
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-02 19:30:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,17 @@
 class Person
-  def name=("person")
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_name)
+    @job = job_name
+  end
+  
+  def job
+    @job
+  end
+end
